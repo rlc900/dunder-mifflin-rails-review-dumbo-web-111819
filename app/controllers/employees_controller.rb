276fdbfe531aebc0cfb9504746ab02e284f2e2aa
Rianna Cleary <rianna.cleary@gmail.com>
2019-12-17 18:15:56 -0500

    our pair lab

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,31 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+
+    @employee = Employee.create(params.require(:employee).permit(:dog_id,:first_name,:last_name,:alias, :title, :office))
+
+    redirect_to employee_path(@employee)
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+   @employee = Employee.find(params[:id])
+   @employee.update(params.require(:employee).permit(:dog_id,:first_name,:last_name,:alias, :title, :office))
+   redirect_to employees_path
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,9 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+
+  # validates :title, :alias presence: :true
+  # validates :title, uniqueness: :true
+  # validates :alias, uniqueness: :true
+
+
 end
